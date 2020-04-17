b90deb05bcd9ca17621551d5df3abd90d246eaff
AlejandroFuster <9201111+AlejandroFuster@users.noreply.github.com>
2020-04-17 19:06:45 +0000

    Update run.R

--- run.R
+++ run.R
@@ -6,6 +6,17 @@
 #
 #    http://shiny.rstudio.com/
 #
+
+my_packages = c("readr", "dplyr", "ggplot2","reshape2","polyreg","pracma","scales","DT")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
+
 port <- Sys.getenv('PORT')
 
 library(readr)
